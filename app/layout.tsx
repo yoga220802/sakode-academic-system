44798bd5a9648eda944988e54027c9b5c57a298f
yoga220802 <yogaagustiansyah01@gmail.com>
2026-06-20 14:07:22 +0700

    Refactor code structure for improved readability and maintainability

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,27 @@ export const metadata: Metadata = {
   title: "Sistem Akademik Sakode Academy",
   description:
     "Portal pembelajaran kursus IT terintegrasi yang dirancang untuk memfasilitasi pendaftaran peserta, kelas trial, modul belajar, serta manajemen dan penjadwalan mentor secara cerdas.",
+  openGraph: {
+    title: "Sistem Akademik Sakode Academy",
+    description:
+      "Portal pembelajaran kursus IT terintegrasi yang dirancang untuk memfasilitasi pendaftaran peserta, kelas trial, modul belajar, serta manajemen dan penjadwalan mentor secara cerdas.",
+    images: [
+      {
+        url: "/icon-banner.png",
+        width: 1200,
+        height: 630,
+        alt: "Sistem Akademik Sakode Academy Banner",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Sistem Akademik Sakode Academy",
+    description:
+      "Portal pembelajaran kursus IT terintegrasi yang dirancang untuk memfasilitasi pendaftaran peserta, kelas trial, modul belajar, serta manajemen dan penjadwalan mentor secara cerdas.",
+    images: ["/icon-banner.png"],
+  },
 };
 
 export default function RootLayout({
@@ -30,7 +51,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
       suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
         <Providers>{children}</Providers>
       </body>
     </html>
